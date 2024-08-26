0fcc0839646d53ac555b32546c59f1f31f56cab6
cvizcain <estudiosvizcaino@gmail.com>
2024-08-26 19:57:02 +0200

    Marina1

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,46 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 09:04:18 by cvizcain          #+#    #+#             */
-/*   Updated: 2024/08/03 09:04:23 by cvizcain         ###   ########.fr       */
+/*   Updated: 2024/08/25 21:07:09 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int	ft_argcount(const char* s)
+{
+	int argcount;
+
+	argcount = 0;
+	while (*s)
+	{
+		if (*s == '%')
+			++argcount;
+		++s;
+	}
+	printf("Arguments count: %i", argcount);
+
+	return argcount;
+}
+
+int	ft_printf(const char* s, ...)
+{
+	int args;
+	va_list lista;
+	
+	args = 0;
+	args = ft_argcount(s);
+	while (s++)
+	va_start(lista, s);
+	
+	write(1, s, sizeof(s) + 2);
+	return (1);
+}
+
+int	main(void)
+{
+	ft_argcount("%Ho%la mu%ndo");
+	return (0);
+}
