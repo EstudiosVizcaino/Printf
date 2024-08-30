b6598c164e8ab182a5d0975982ad5e30c583774b
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2024-08-31 00:40:44 +0200

    Checked!

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:21:12 by cvizcain          #+#    #+#             */
-/*   Updated: 2024/08/31 00:36:31 by cvizcain         ###   ########.fr       */
+/*   Updated: 2024/08/31 00:40:37 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@
 
 void	ft_putchar(int c, int *charcount)
 {
-	*charcount = write (1, &c, 1);
+	*charcount += write (1, &c, 1);
 }
