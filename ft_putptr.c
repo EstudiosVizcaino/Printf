a77fdbafd624c047bc61c0f079c895f665e88b21
cvizcain <estudiosvizcaino@gmail.com>
2024-09-01 20:33:08 +0200

    (nil)

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:22:53 by cvizcain          #+#    #+#             */
-/*   Updated: 2024/08/31 00:02:06 by cvizcain         ###   ########.fr       */
+/*   Updated: 2024/09/01 20:31:02 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,16 @@ void	ft_putptr_base(unsigned long int nbr, int *charcount)
 void	ft_putptr(void *nbr, int *charcount)
 {
 	unsigned long int	i;
+	if (nbr == NULL)
+	{
+		ft_putstr("(nil)", charcount);
+		return;
+	}
+	else
+	{
 
-	i = (unsigned long int) nbr;
-	ft_putstr("0x", charcount);
-	ft_putptr_base(i, charcount);
+		i = (unsigned long int) nbr;
+		ft_putstr("0x", charcount);
+		ft_putptr_base(i, charcount);
+	}
 }
